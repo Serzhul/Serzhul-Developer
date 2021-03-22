7fc73d548421386969ff98e031d969cdaec2c975
Serzhul <wilshere917@gmail.com>
2021-03-23 00:28:40 +0900

    Rebuilding Test1

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `Serzhul's Developing`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `Serzhul`,
+  introduction: `I'm a Developer growing Everyday `,
+  siteUrl: `https://serzhul.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `Serzhul`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `sdwzzng@naver.com`, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/serzhul.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'Serzhul', // Your disqus-short-name. check disqus.com.
+    utterances: 'Serzhul/Serzhul-Developer', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
