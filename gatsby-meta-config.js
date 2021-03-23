2ed813894b4e643ee3f84b56a78b084ba7fbb042
Serzhul <wilshere917@gmail.com>
2021-03-23 22:06:55 +0900

    Rebuilding Test2

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `Serzhul's Developing`,
-  description: `Blog posted about ...`,
+  title: `Serzhul`,
+  description: `The Story of Growing`,
   author: `Serzhul`,
-  introduction: `I'm a Developer growing Everyday `,
+  introduction: `I'm a Developer Growing Everyday `,
   siteUrl: `https://serzhul.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
@@ -14,14 +14,14 @@ module.exports = {
   icon: `content/assets/serzhul.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: 'Serzhul', // Your disqus-short-name. check disqus.com.
+    disqusShortName: 'serzhul-io', // Your disqus-short-name. check disqus.com.
     utterances: 'Serzhul/Serzhul-Developer', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'serzhul',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
